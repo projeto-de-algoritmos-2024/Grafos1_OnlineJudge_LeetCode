f1403e2ac55058d8a839bc37ad4abf098bbc00bd
anaaroch <sqscamposuni@gmail.com>
2024-04-10 10:14:27 -0300

    fix: primeira submissão ficou em 95%

--- midnight-cowboy.py
+++ midnight-cowboy.py
@@ -1,11 +1,12 @@
-def bfs(graph, start, end):
+def bfs(graph, start, end, obstacle):
     queue = [(start, [start])]
     paths = []
 
     while queue:
         (node, path) = queue.pop(0)
-        if node == end:
+        if node == end and obstacle not in path:
             paths.append(path)
+            break
         for neighbour in graph[node]:
             if neighbour not in path:
                 new_path = list(path)
@@ -17,18 +18,18 @@ def bfs(graph, start, end):
 def main ():
     N, A, B, C = map(int, input().split())
 
-    graph = [[] for _ in range(0, N + 1)]
-    for _ in range(0, N-1):
+    graph = [[] for _ in range(N + 1)]
+    for _ in range(N-1):
         X, Y = map(int, input().split())
         
         graph[X].append(Y)
         graph[Y].append(X)
     
-    pathsAforB = bfs(graph, A, B)
-    pathsAforC = bfs(graph, A, C)
+    pathsAforB = bfs(graph, A, B, C)
+    pathsAforC = bfs(graph, A, C, B)
 
-    probability = format((len(pathsAforB) / 1) / (len(pathsAforB) + len(pathsAforC)), ".6f")
-    print(probability)
-    
+    probability = len(pathsAforB) / (len(pathsAforB) + len(pathsAforC))
+    print(f"{probability:.6f}")
+
 if __name__ == "__main__":
     main()
