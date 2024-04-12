064b7b9ebf5fbba7fbb747a4bea84357d94366bf
CarolCoCe <anaipva@gmail.com>
2024-04-12 20:45:27 -0300

    fix: Ainda com runtime error mas na teoria completo

--- 913. Cat and Mouse/catAndMouse.py
+++ 913. Cat and Mouse/catAndMouse.py
@@ -1,22 +1,47 @@
+def bfs (graph, startPosition, AimedPosition, visited):
+    fila = [startPosition]
+    looked = set([startPosition])
+    
+    while fila:
+        u = fila.pop(0)
+        if u == AimedPosition:
+            return looked[1]
+        
+        for vizinho in graph[u]:
+            if (vizinho not in visited) and (vizinho not in looked):
+                looked.add(vizinho)
+                fila.append(vizinho)
 class Solution:
+                    
     def catMouseGame(self, graph: List[List[int]]) -> int:
         
         mousePosition = 1
         catPosition = 2
         holePosition = 0
-        
-        #mouse turn
-        #acha o caminho mais curto
-        #vai pro seu vizinho na direcao do caminho mais curto
+
         mouseVisited = []
+        catVisited = []
+        catVisited.append(holePosition)
         
-    def bfs (graph, visited, startPosition, AimedPosition):
-        fila.append(startPosition)
-        visited.append(startPosition)
+        result = -1
         
-        while fila:
-            u = fila.pop(0)
-            for each vizinho in graph[u]:
-                if vizinho is not in visited:
+        while (result == -1):
+            
+            mousePosition = bfs(graph,mousePosition, holePosition, mouseVisited)
+            catPosition = bfs(graph,catPosition, mousePosition, catVisited)
+            
+            mouseVisited.append(mousePosition)
+            catVisited.append(catPosition)
+            
+            if mousePosition == holePosition:
+                result = 1
+            elif mousePosition == catPosition:
+                result = 2
+            elif (len(graph) == len(mouseVisited)) or (len(graph) == len(catVisited)):
+                result = 0
+            
+        return result
+        
+
                     
         
