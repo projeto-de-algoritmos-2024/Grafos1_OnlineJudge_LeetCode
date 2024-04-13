8796e2571df2e2519f7902b0b81ee6cc7af5bb9c
CarolCoCe <anaipva@gmail.com>
2024-04-12 22:05:06 -0300

    fix: arrumando as condicoes de empate, mas ainda não deu certo

--- 913. Cat and Mouse/catAndMouse.py
+++ 913. Cat and Mouse/catAndMouse.py
@@ -1,16 +1,17 @@
-def bfs (graph, startPosition, AimedPosition, visited):
+def bfs (graph, startPosition, aimedPosition, visited):
     fila = [startPosition]
-    looked = [startPosition]
+    looked = {startPosition}
     
     while fila:
-        u = fila.pop(0)
-        if u == AimedPosition:
-            return looked[1]
+        noAtual = fila.pop(0)
+        if noAtual == aimedPosition:
+            return looked
         
-        for vizinho in graph[u]:
+        for vizinho in graph[noAtual]:
             if (vizinho not in visited) and (vizinho not in looked):
-                looked.append(vizinho)
+                looked.add(vizinho)
                 fila.append(vizinho)
+    return None
 class Solution:
                     
     def catMouseGame(self, graph: List[List[int]]) -> int:
@@ -19,27 +20,33 @@ class Solution:
         catPosition = 2
         holePosition = 0
 
-        mouseVisited = []
-        catVisited = []
-        catVisited.append(holePosition)
+        mouseVisited = set()
+        catVisited = set()
+        catVisited.add(holePosition)
         
         result = -1
         
         while (result == -1):
             
-            mousePosition = bfs(graph,mousePosition, holePosition, mouseVisited)
-            catPosition = bfs(graph,catPosition, mousePosition, catVisited)
+            mouseLooked = bfs(graph,mousePosition, holePosition, mouseVisited)
             
-            mouseVisited.append(mousePosition)
-            catVisited.append(catPosition)
-            
-            if mousePosition == holePosition:
+            if mouseLooked:
                 result = 1
-            elif mousePosition == catPosition:
-                result = 2
-            elif (len(graph) == len(mouseVisited)) or (len(graph) == len(catVisited)):
-                result = 0
-            
+            else:
+                catLooked = bfs(graph,catPosition, mousePosition, catVisited)
+                if catLooked & mouseLooked:
+                    result = 2
+                else:
+                    mouseVisited.update(mouseLooked or set([mousePosition]))
+                    catVisitedx.update(catLooked or set([catPosition]))
+                    
+                    if mousePosition in mouseVisited and mouseLooked is None:
+                        result = 0
+                    elif catPosition in catVisited and catLooked is None:
+                        result = 0
+                        
+                    mousePosition = (catPosition if result != 2 else mousePosition)
+                    
         return result
         
 
