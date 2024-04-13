7f828b54d77785f623d3b2ca2d699e9a70325e03
CarolCoCe <anaipva@gmail.com>
2024-04-13 01:59:03 -0300

    fix: Ta dando wa no 886 mas tá rodando

--- 886. Possible Bipartition.py
+++ 886. Possible Bipartition.py
@@ -1,25 +1,27 @@
 class Solution:
     def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
-        layer = 0 #groups par ou impar
+        layers = [0] * (n+1) #groups
         fila = []
         visited = []
         
-        for i in dislikes:
-            if i not in visited:
-                fila.append(i)
-                fila.append(layer)
+        for i in range(1, n+1):
+            
+            if layers[i] == 0: #usando o vetor pra saber 
+                               # quais já foram visitados tb
+                fila = [i]
+                layers[i] = 1
+                
                 while fila:
                     noAtual = fila.pop(0)
-                    layerAtual = fila.pop(0)
-                    visited.append(noAtual)
+                    layerAtual = layers[noAtual]
                     
-                    for vizinho in dislikes[noAtual]:
-                        if (vizinho not in visited):
-                            visited.append(vizinho)
+                    for vizinho in dislikes[noAtual - 1]:
+                        if (layers[noAtual] == 0):
+                            layers[vizinho] = -1 * layerAtual
                             fila.append(vizinho)
-                            fila.append(layerAtual + 1)
-                        elif( noAtual == fila[vizinho + 1]):
-                            return false
+                            
+                        elif(layers[vizinho] == layerAtual):
+                            return False
         
-        return true
+        return True
         
